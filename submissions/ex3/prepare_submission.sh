18823a1664c3a6c7f1bd8622b1e51af71e7a9b0c
omriporat <omri.porat2@mail.huji.ac.il>
2023-05-18 16:19:55 +0300

    copy correct files

--- submissions/ex3/prepare_submission.sh
+++ submissions/ex3/prepare_submission.sh
@@ -5,15 +5,15 @@ cd "$HOME/Uni/IML/IML.HUJI/submissions/ex3"
 
 base_path="$HOME/Uni/IML/IML.HUJI"
 
-cp "$base_path/IMLearn/utils/utils.py" .
+cp "$base_path/utils.py" .
 cp "$base_path/IMLearn/metrics/loss_functions.py" .
 cp "$base_path/exercises/classifiers_evaluation.py" .
 cp "$base_path/IMLearn/learners/classifiers/gaussian_naive_bayes.py" .
 cp "$base_path/IMLearn/learners/classifiers/linear_discriminant_analysis.py" .
 cp "$base_path/IMLearn/learners/classifiers/perceptron.py" .
 
-cp "/Users/omriporat/Downloads/IML - Ex 3.pdf" .
-mv "IML - Ex 2.pdf" "Answers.pdf"
+cp "$HOME/Downloads/IML - Ex 3.pdf" .
+mv "IML - Ex 3.pdf" "Answers.pdf"
 
 rm "ex3_$id.tar"
 
